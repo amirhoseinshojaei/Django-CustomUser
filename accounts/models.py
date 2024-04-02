e1434b4b50fc3fb94e38c2ab7fb925fce061ff91
amirhosein <amirhoseinbu80@gmail.com>
2024-04-03 02:15:50 +0330

    create custom user

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser,AbstractBaseUser,BaseUserManager,PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+from django.utils import timezone
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
@@ -33,3 +34,24 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('SuperUser must have is_superuser=True')
         return self.create_user(email,name,last_name,age,gender,password=None,**extra_fields)
 
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    Gender_Type = (
+        ('male','male'),
+        ('female','female')
+    )
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.CharField(max_length=10,choices=Gender_Type)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']     # description = A list of the field names that will be prompted for when creating a user via the createsuperuser management command. The user will be prompted to supply a value for each of these fields.
+
+    def __str__(self):
+        return f'User is {self.email}'
